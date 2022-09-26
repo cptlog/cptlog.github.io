faf81aa1b942d6ec889dcf3c1bf4f558b334352f
ioyli <90900819+ioyli@users.noreply.github.com>
2022-09-26 20:10:32 +0300

    -

--- src/components/router.js
+++ src/components/router.js
@@ -3,9 +3,13 @@ import { Switch, Route, Router } from 'wouter'
 import Home from '../pages/home'
 import About from '../pages/about'
 
-export default () => (
-    <Switch>
-        <Route path='/' component={Home} />
-        <Route path='/about' component={About} />
-    </Switch>
-)
+const Router = () => {
+    return (
+        <Switch>
+            <Route path='/' component={Home} />
+            <Route path='/about' component={About} />
+        </Switch>
+    )
+}
+
+export default Router
