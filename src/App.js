d6bd14572547588a7b22a5965b6455b940b560a2
ioyli <90900819+ioyli@users.noreply.github.com>
2022-09-26 17:48:48 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import './style/main.css'
 
-function App() {
+const links = [
+  {
+    name: 'home',
+    url: '/a',
+    key: 1
+  },
+  {
+    name: 'bleugh',
+    url: '/b',
+    key: 2
+  }
+]
+
+const Link = ({link}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <a href={link.url}><li>{link.name}</li></a>
+  )
 }
 
-export default App;
+const Navigation = ({links}) => {
+  return (
+    <ul className='navigation'>
+      {links.map(link =>
+        <Link key={link.key} link={link} />
+      )}
+    </ul>
+  )
+}
+
+const Header = ({title}) => {
+  return (
+    <div className='container header'>
+      {title}
+    </div>
+  )
+}
+
+const Left = () => {
+  return (
+    <div className='left'>
+      <Navigation links={links} />
+    </div>
+  )
+}
+
+const Center = () => {
+  return (
+    <div className='container center'>
+      under construction <a href="/">link</a>
+    </div>
+  )
+}
+
+const App = () => {
+  const title = 'cptlog'
+
+  return (
+    <div className='app'>
+      <Header title={title} />
+      <Left />
+      <Center />
+    </div>
+  )
+}
+
+export default App
